235315cc54d10fd2f0b97156c505c3ae36b4147b
MacoTasu <makomacomako@me.com>
2014-12-11 20:55:53 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Script::Nohup - It's new $module
+
+# SYNOPSIS
+
+    use Script::Nohup;
+
+# DESCRIPTION
+
+Script::Nohup is ...
+
+# LICENSE
+
+Copyright (C) MacoTasu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+MacoTasu <makomacomako@me.com>
---
+++ lib/Script/Nohup.pm
@@ -0,0 +1,39 @@
+package Script::Nohup;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Script::Nohup - It's new $module
+
+=head1 SYNOPSIS
+
+    use Script::Nohup;
+
+=head1 DESCRIPTION
+
+Script::Nohup is ...
+
+=head1 LICENSE
+
+Copyright (C) MacoTasu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+MacoTasu E<lt>makomacomako@me.comE<gt>
+
+=cut
+
